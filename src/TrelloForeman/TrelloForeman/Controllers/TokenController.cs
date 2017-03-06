fd1027b0761a8b312c7a7b2908c780fdcf813a86
supershowwei <supershowwei@gmail.com>
2017-03-06 15:44:54 +0800

    complete TokenController.Index()

---
+++ src/TrelloForeman/TrelloForeman/Controllers/TokenController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace TrelloForeman.Controllers
+{
+    [RoutePrefix("token")]
+    public class TokenController : Controller
+    {
+        // GET: Token
+        [Route]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
