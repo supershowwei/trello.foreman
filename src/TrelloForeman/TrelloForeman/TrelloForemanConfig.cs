e895cc41673d69aca3169b3afab8729394bc79d2
supershowwei <supershowwei@gmail.com>
2017-03-07 23:18:27 +0800

    add persistence 機制

--- src/TrelloForeman/TrelloForeman/TrelloForemanConfig.cs
+++ src/TrelloForeman/TrelloForeman/TrelloForemanConfig.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Caching;
-using System.Web;
+using System.Web.Hosting;
 using System.Xml.Linq;
 
 namespace TrelloForeman
@@ -12,8 +12,9 @@ namespace TrelloForeman
     {
         public static readonly TrelloForemanConfig Instance = new TrelloForemanConfig();
         private static readonly string SecretDocumentKey = "Secret";
+        private readonly object lockedObject = new object();
         private readonly ObjectCache objectCache = MemoryCache.Default;
-        private readonly ConcurrentQueue<string> workerIdQueue = new ConcurrentQueue<string>();
+        private List<string> workers = new List<string>();
 
         private TrelloForemanConfig()
         {
@@ -26,6 +27,9 @@ namespace TrelloForeman
         public string ToDoListId => this.SecretDocument.Root.Element("ToDoList").Attribute("Id").Value;
 
         public string DingtalkWebhookUrl => this.SecretDocument.Root.Element("Dingtalk").Element("WebhookUrl").Value;
+
+        private string RemainingWorkersFilePath
+            => Path.Combine(HostingEnvironment.MapPath("~/App_Data"), "remaining_workers");
 
         private XDocument SecretDocument
         {
@@ -40,40 +44,55 @@ namespace TrelloForeman
             }
         }
 
-        private string[] WorkerIdList
-            => this.SecretDocument.Root.Element("Workers").Elements().Select(w => w.Attribute("Id").Value).ToArray();
-
         public string FetchOneWorker()
         {
-            if (this.workerIdQueue.Count == 0)
-            {
-                var workerIdList = this.WorkerIdList;
-
-                var procession = Enumerable.Range(0, workerIdList.Length).ToList();
-
-                var random = new Random(Guid.NewGuid().GetHashCode());
-                while (procession.Count > 0)
-                {
-                    var index = random.Next(procession.Count);
-
-                    this.workerIdQueue.Enqueue(workerIdList[procession[index]]);
-
-                    procession.RemoveAt(index);
-                }
-            }
-
             string workerId;
-            while (!this.workerIdQueue.TryDequeue(out workerId))
+            lock (this.lockedObject)
             {
-                // 一定要取到值
+                if (this.workers.Count == 0)
+                {
+                    this.CreateWorkers();
+                }
+
+                workerId = this.workers.First();
+
+                this.workers.RemoveAt(0);
+
+                File.WriteAllText(this.RemainingWorkersFilePath, string.Join("\r\n", this.workers));
             }
 
             return workerId;
         }
 
+        private void CreateWorkers()
+        {
+            this.workers = File.ReadAllLines(this.RemainingWorkersFilePath).ToList();
+
+            if (this.workers.Count == 0)
+            {
+                var workerIdList =
+                    this.SecretDocument.Root.Element("Workers")
+                        .Elements()
+                        .Select(w => w.Attribute("Id").Value)
+                        .ToArray();
+
+                var numbers = Enumerable.Range(0, workerIdList.Length).ToList();
+
+                var random = new Random(Guid.NewGuid().GetHashCode());
+                while (numbers.Count > 0)
+                {
+                    var index = random.Next(numbers.Count);
+
+                    this.workers.Add(workerIdList[numbers[index]]);
+
+                    numbers.RemoveAt(index);
+                }
+            }
+        }
+
         private void LoadSecretDocument()
         {
-            var file = Path.Combine(HttpContext.Current.Server.MapPath("~/App_Data"), "secret.xml");
+            var file = Path.Combine(HostingEnvironment.MapPath("~/App_Data"), "secret.xml");
 
             var cacheItemPolicy = new CacheItemPolicy();
             cacheItemPolicy.ChangeMonitors.Add(new HostFileChangeMonitor(new[] { file }));
